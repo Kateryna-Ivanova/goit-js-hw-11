c2e8e411d19a0b48d0534a88b4dc7ad6d0d22543
Kateryna-Ivanova <129738301+Kateryna-Ivanova@users.noreply.github.com>
2024-01-07 17:57:05 +0000

    Deploying to gh-pages from @ Kateryna-Ivanova/goit-js-hw-11@2f9008c6e0bd3570ff03f4de85c9c7fc88b38836 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,23 +1,23 @@
-import{S as l,i as c}from"./assets/vendor-9310f15c.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const u=document.querySelector(".form"),d=document.querySelector(".gallery"),f=document.querySelector(".text-input"),i=document.querySelector(".loader"),m=new l(".gallery a",{captionsData:"alt",overlayOpacity:.5});function p(){c.error({message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"#FAFAFB",backgroundColor:"#EF4040",position:"topRight",close:!1,closeOnClick:!0})}u.addEventListener("submit",g);function g(o){o.preventDefault(),i.style.display="block";const s=new URLSearchParams({key:"41488002-513c6a9a4c115eae6a99045d3",q:f.value,image_type:"photo",orientation:"horizontal",safesearch:!0});fetch(`https://pixabay.com/api/?${s}`).then(r=>{if(i.style.display="none",!r.ok)throw new Error(r.status);return r.json()}).then(r=>{if(r.hits.length===0)return p();const n=r.hits.reduce((e,t)=>e+h(t),"");d.innerHTML=n,m.refresh()}).catch(r=>{showAlert(r.toString())})}function h(o){return`<li>
-    <a href="${o.largeImageURL}">
-        <img src="${o.webformatURL}" alt="${o.tags}">
+import{S as c,i as u}from"./assets/vendor-9310f15c.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&s(a)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();const d=document.querySelector(".form"),i=document.querySelector(".gallery"),l=document.querySelector(".loader"),f=new c(".gallery a",{captionsData:"alt",overlayOpacity:.5});function m(){u.error({message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"#FAFAFB",backgroundColor:"#EF4040",position:"topRight",close:!1,closeOnClick:!0})}d.addEventListener("submit",p);function p(r){r.preventDefault(),l.style.display="block",i.innerHTML="";const o=r.target.elements.search.value,n=new URLSearchParams({key:"41488002-513c6a9a4c115eae6a99045d3",q:o,image_type:"photo",orientation:"horizontal",safesearch:!0});fetch(`https://pixabay.com/api/?${n}`).then(s=>{if(l.style.display="none",!s.ok)throw new Error(s.status);return s.json()}).then(s=>{if(s.hits.length===0)return i.innerHTML="",m();const e=s.hits.reduce((t,a)=>t+g(a),"");i.innerHTML=e,f.refresh()}).catch(s=>{showAlert(s.toString())})}function g(r){return`<li>
+    <a href="${r.largeImageURL}">
+        <img src="${r.webformatURL}" alt="${r.tags}">
     </a>
     <div class="info">
         <div class="image-info">
             <span>Likes</span>
-            <span class="image-value">${o.likes}</span>
+            <span class="image-value">${r.likes}</span>
         </div>
         <div class="image-info">
             <span>Views</span>
-            <span class="image-value">${o.views}</span>
+            <span class="image-value">${r.views}</span>
         </div>
         <div class="image-info">
             <span>Comments</span>
-            <span class="image-value">${o.comments}</span>
+            <span class="image-value">${r.comments}</span>
         </div>
         <div class="image-info">
             <span>Downloads</span>
-            <span class="image-value">${o.downloads}</span>
+            <span class="image-value">${r.downloads}</span>
         </div>
     </div>
     </li>`}
