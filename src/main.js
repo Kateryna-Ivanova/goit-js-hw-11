d361f4c04f367e2c4ff2b673a58faf6e85b5dde6
Kateryna-Ivanova <katerinadialog@gmail.com>
2024-01-07 05:33:26 +0200

    created a gallery
    first check

--- src/main.js
+++ src/main.js
@@ -1,96 +1,93 @@
-// import iziToast from "izitoast";
-// import "izitoast/dist/css/iziToast.min.css";
 
-// import SimpleLightbox from "simplelightbox";
-// import "simplelightbox/dist/simple-lightbox.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-const fetchUsersBtn = document.querySelector(".form");
+const form = document.querySelector(".form");
 const gallery = document.querySelector(".gallery");
 const textInput = document.querySelector('.text-input')
-const modal = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-});
-
 const loader = document.querySelector('.loader');
-  loader.style.display = 'none';
 
-
-fetchUsersBtn.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const usersValue = textInput.value;
-
-  gallery.innerHTML = '';
-  textInput.value = '';
-  loader.style.display = 'block';
-
-  const searchParams = new URLSearchParams({
-  key: '41659492-ec5a278d39a7ff9431707ab98',
-  q: usersValue,
-  image_type: 'photo',
-  orientation: 'horizontal',
-  safesearch: true
+// Використання галереї SimpleLightbox(Зображення відкриваються як модальне вікно)
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    overlayOpacity: 0.5,
 });
 
-  fetch(`https://pixabay.com/api/?${searchParams}`)
-    .then((response) => {
-      loader.style.display = 'none';
-      
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-  .then((data) => {
-      if (data.hits.length === 0) {
-        iziToast.error({
-          message: 'Sorry, there are no images matching your search query. Please try again!',
-          messageColor: '#FAFAFB',
-          backgroundColor: '#EF4040',
-          position: 'topRight'
-        });
-        return;
-    }
-      const imagesHTML = data.hits.reduce((html, image) => {
-      return html + imageCard(image);
-      }, '');
+// Використання iziToast (Вспливаюче повідомлення)
+function showMessage() {
+    iziToast.error({
+    message: 'Sorry, there are no images matching your search query. Please try again!',
+    messageColor: '#FAFAFB',
+    backgroundColor: '#EF4040',
+    position: 'topRight',
+    close: false,
+    closeOnClick: true,
+    });
     
-    gallery.innerHTML = imagesHTML;
-    modal.refresh();
+}
 
-    })
-  .catch((error) => {
-		  showAlert(error.toString());
-  })
-});
+form.addEventListener('submit', fetchImages);
 
+function fetchImages(event) {
+    // прибираємо дію за замовченням
+    event.preventDefault();
+    // відміняємо приховування loader
+    loader.style.display = 'block';
+    // Створення параметрів пошуку Pixabay
+    const searchParams = new URLSearchParams({
+        key: '41488002-513c6a9a4c115eae6a99045d3',
+        q: textInput.value ,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+    });
+    // Отримання даних з Pixabay за параметрами
+    fetch(`https://pixabay.com/api/?${searchParams}`)
+        .then((response) => {
+            loader.style.display = 'none';
+            if (!response.ok) { throw new Error(response.status); }
+            return response.json();
+        })
+        .then((data) => {
+            // (Якщо data.hits.length дорівнює 0, це означає, що у відповіді немає зображень)
+            if (data.hits.length === 0) {
+                return showMessage();
+            }
+            const renderImg = data.hits.reduce((html, image) => {
+                return html + imageCard(image);
+            }, '');
+            gallery.innerHTML = renderImg;
+            // оновлюємо вміст
+            lightbox.refresh();
+        })
+        .catch((error) => {
+            showAlert(error.toString());
+        });
+}
 function imageCard(images) {
-  return `<li>
-      <a href="${images.largeImageURL}">
+    return `<li>
+    <a href="${images.largeImageURL}">
         <img src="${images.webformatURL}" alt="${images.tags}">
-      </a>
-      <div class="info">
+    </a>
+    <div class="info">
         <div class="image-info">
-          <span>Likes</span>
-          <span class="image-value">${images.likes}</span>
+            <span>Likes</span>
+            <span class="image-value">${images.likes}</span>
         </div>
         <div class="image-info">
-          <span>Views</span>
-          <span class="image-value">${images.views}</span>
+            <span>Views</span>
+            <span class="image-value">${images.views}</span>
         </div>
         <div class="image-info">
-          <span>Comments</span>
-          <span class="image-value">${images.comments}</span>
+            <span>Comments</span>
+            <span class="image-value">${images.comments}</span>
         </div>
         <div class="image-info">
-          <span>Downloads</span>
-          <span class="image-value">${images.downloads}</span>
+            <span>Downloads</span>
+            <span class="image-value">${images.downloads}</span>
         </div>
-      </div>
-    </li>
-  `;
+    </div>
+    </li>`;
 };
